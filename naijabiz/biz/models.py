be1fe8666a0ca345c072ea7a270d72f95bb5a90b
ayinla <dejiolawoye@outlook.com>
2015-11-04 18:34:44 -0800

    added biz and review model

--- naijabiz/biz/models.py
+++ naijabiz/biz/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Biz(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    phone = models.CharField(max_length=13)
+
+class Review(models.Model):
+    review = models.CharField(max_length=100)
